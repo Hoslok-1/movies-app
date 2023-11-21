f2a85fc15c2bfe8fee1217c7b81c5cf60bbce972
yung42 <altyung42@gmail.com>
2023-11-22 00:39:26 +0530

    Initial commit for backend movies app

---
+++ src/main/java/com/example/moviesappspring/MoviesAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviesappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviesappspring/MoviesAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviesappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
