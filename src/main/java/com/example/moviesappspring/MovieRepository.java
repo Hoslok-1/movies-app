6a79c59563ca69b266e019402ad7a2038a988d23
yung42 <altyung42@gmail.com>
2023-11-22 19:37:48 +0530

    created 2 more endpoints which can search movies by their imdbId and their genres

--- src/main/java/com/example/moviesappspring/MovieRepository.java
+++ src/main/java/com/example/moviesappspring/MovieRepository.java
@@ -4,6 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    public Optional<Movie> findByImdbId(String imdbId);
+    public Optional<List<Movie>> findByGenres(String genre);
+
 }
--- src/main/java/com/example/moviesappspring/MovieService.java
+++ src/main/java/com/example/moviesappspring/MovieService.java
@@ -1,9 +1,12 @@
 package com.example.moviesappspring;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,4 +15,15 @@ public class MovieService {
     public List<Movie> allMovies(){
         return movieRepository.findAll();
     }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findByImdbId(imdbId);
+    }
+
+    public Optional<List<Movie>> moviesFromGenres(String genre){
+        return movieRepository.findByGenres(genre);
+    }
+
+
+
 }
